483308a0a855675530a1fccf3c0808f02734204c
Will Lorand <bill@milemarker3.org>
2020-07-11 11:54:08 -0700

    added posters, finalized v1 styles

--- src/data/filmNoirData.js
+++ src/data/filmNoirData.js
@@ -4,18 +4,29 @@ const filmNoirData = [
     year: '1944',
     quote:
       'How could I have known that murder can sometime smell like honeysuckle?',
+    posterUrl:
+      'http://www.milemarker3.org/images/noir-posters/double-indemnity.jpg',
   },
   {
     title: 'Out of the Past',
     year: '1947',
     quote: 'How big a chump can you get to be? I was finding out.',
+    posterUrl:
+      'http://www.milemarker3.org/images/noir-posters/out-of-the-past.jpg',
   },
   {
     title: 'Gun Crazy',
     year: '1950',
     quote: "She ain't the type that makes a happy home",
+    posterUrl: 'http://www.milemarker3.org/images/noir-posters/gun-crazy.jpg',
   },
-  { title: 'Touch of Evil', year: '1958', quote: 'Something Wrong, Vargas?' },
+  {
+    title: 'Touch of Evil',
+    year: '1958',
+    quote: 'Something Wrong, Vargas?',
+    posterUrl:
+      'http://www.milemarker3.org/images/noir-posters/touch-of-evil.jpg',
+  },
 ];
 
 export default filmNoirData;
